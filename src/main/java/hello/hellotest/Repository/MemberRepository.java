6a9891ddc4315eb823726b7b131f12dbf3737fed
hanwoosung <han.woosung@neut.co.jp>
2023-02-15 15:00:43 +0900

    Create Member_Domain,Member_Repository,Member_test

---
+++ src/main/java/hello/hellotest/Repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellotest.Repository;
+
+import hello.hellotest.Domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
